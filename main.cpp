0a9b8c8fa8530186ce64faa93aa9878b9ad614e7
peducation <kaur75606@gmail.com>
2020-11-18 17:07:12 +0100

    The class program of sphere

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include "area_volume.h"
+
+
+
+
+int main() {
+    areaVolume area1, volume1;
+    //area1.setdata(2,3, 4);
+    area1.getdata1();
+    cout <<"Area is: " <<area1.area() <<endl;
+
+
+    volume1.getdata2();
+    cout <<"Volume is: " <<volume1.volume() <<endl;
+
+    return 0;
+}

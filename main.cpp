68a02ee4b497f710c8fd3667edf0bb78bad9de17
peducation <kaur75606@gmail.com>
2020-11-18 19:47:51 +0100

    The class program in c++

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
+//This program is about calculating area an volume using class
+//Created by Navjot Kaur
+//
 #include <iostream>
 using namespace std;
 #include "area_volume.h"
 
-
-
-
 int main() {
-    areaVolume area1, volume1;
-    //area1.setdata(2,3, 4);
+    areaVolume area1, volume1; //defining objects
+
     area1.getdata1();
     cout <<"Area is: " <<area1.area() <<endl;
 
-
     volume1.getdata2();
-    cout <<"Volume is: " <<volume1.volume() <<endl;
+    cout <<"Volume is: " <<volume1.volume1() <<endl;
 
     return 0;
 }

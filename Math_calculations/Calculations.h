1f077edef29d83629963d8ed701d997ddb48fbf5
peducation <kaur75606@gmail.com>
2020-12-15 15:34:27 +0100

    The c++ program for math calculations

---
+++ Math_calculations/Calculations.h
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------------------------
+File Name:        Calculations.h(This file contains the header file)
+Description:      The objective of this program is to do math calculations such as adiition, 
+                  subtraction, multiplication and mixed exercises with user's choice.
+Written by:       Navjot Kaur
+Date:             2020-12-07
+-----------------------------------------------------------------------------------------*/
+
+#ifndef MATH_PROGRAM_CALCULATIONS_H
+#define MATH_PROGRAM_CALCULATIONS_H
+
+//defination of functions
+int randomValue1();
+int randomValue2();
+void display();
+void doPractice(int choice);
+void doTest(int choice);
+
+#endif //MATH_PROGRAM_MATHS_H
